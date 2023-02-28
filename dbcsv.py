87834369ba65a41fd939dc9097feaf31a16e2dc9
Lucas <lucaszhengrui@gmail.com>
2023-02-28 16:27:02 +0000

    Commit 20230228

--- dbcsv.py
+++ dbcsv.py
@@ -5,9 +5,28 @@ import sqlite3
 connection = sqlite3.connect('database.db')
 curs = connection.cursor()
 
+# Deleted and created country table
 connection.execute('drop table if exists country')
 print("Exist tables deleted successfully!")
-connection.execute('create table country (id integer, country_name text, city_name text)')
+connection.execute('create table country (id integer PRIMARY KEY, country_name text, city_name text)')
+print("Table country created successfully!")
+
+# Deleted and created city table
+connection.execute('drop table if exists city')
+print("Exist tables deleted successfully!")
+connection.execute('create table city (id integer REFERENCES country(id), gender text, ethnicity_name text)')
+print("Table country created successfully!")
+
+# Deleted and created animal table
+connection.execute('drop table if exists animal')
+print("Exist tables deleted successfully!")
+connection.execute('create table animal (id integer REFERENCES country(id), animal_type text, animals text)')
+print("Table country created successfully!")
+
+# Deleted and created animal table
+connection.execute('drop table if exists humans')
+print("Exist tables deleted successfully!")
+connection.execute('create table humans (id integer REFERENCES country(id), humans text, is_required text, last_updated_date text)')
 print("Table country created successfully!")
 
 # connection.close()
@@ -21,15 +40,22 @@ with open('data.csv', newline='') as d:
         id = int(row[0])
         country_name = row[1]
         city_name = row[2]
-        # gender = row[3]
-        # ethnicity_name = row[4]
-        # animal_type = row[5]
-        # animals = row[6]
-        # humans = row[7]
-        # is_required = row[8]
-        # last_updated_date = row[9]
+        gender = row[3]
+        ethnicity_name = row[4]
+        animal_type = row[5]
+        animals = row[6]
+        humans = row[7]
+        is_required = row[8]
+        last_updated_date = row[9]
 
+        # inserted the data to country table
         curs.execute('INSERT INTO country VALUES (?, ?, ?)', (id, country_name, city_name))
+        # inserted the data to city table
+        curs.execute('INSERT INTO city VALUES (?, ?, ?)',(id, gender, ethnicity_name))
+        # inserted the data to animal table
+        curs.execute('INSERT INTO animal VALUES (?, ?, ?)',(id, animal_type, animals))
+        # inserted the data to humans table
+        curs.execute('INSERT INTO humans VALUES (?, ?, ?, ?)',(id, humans, is_required, last_updated_date))
         connection.commit()
 print("Data inputted done!")
 
--- showdb.py
+++ showdb.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/country')
+@app.route('/')
 def index():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     curs = connection.cursor()
     curs.execute("select * from country")
+    curs.execute("select * from city")
+    curs.execute("select * from animal")
+    curs.execute("select * from humans")
     rows_ = curs.fetchall()
     connection.close()
     return render_template('mainpage.html', rows = rows_)
