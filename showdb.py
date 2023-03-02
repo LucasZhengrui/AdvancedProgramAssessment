44f83f57496b59bace63cb0ffa5b0dfbaf987b98
Lucas <lucaszhengrui@gmail.com>
2023-03-02 22:08:55 +0000

    Combined tables successfully

--- showdb.py
+++ showdb.py
@@ -8,10 +8,14 @@ def index():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     curs = connection.cursor()
-    curs.execute("select * from country")
-    # curs.execute("select * from city")
-    # curs.execute("select * from animal")
-    # curs.execute("select * from humans")
+    curs.execute("select * from country, city where (city.id = country.id and country.id <= 30 and country.country_name = 'China')")
     rows_ = curs.fetchall()
     connection.close()
     return render_template('mainpage.html', rows = rows_)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# curs.execute("select * from city")
+# curs.execute("select * from animal")
+# curs.execute("select * from humans")
