9bb681d52eff6388e78ad9cc61128ee923c3a2a4
Lucas <lucaszhengrui@gmail.com>
2023-03-03 04:41:04 +0000

    Updated for showing linked tables

--- showdb.py
+++ showdb.py
@@ -5,17 +5,50 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('mainpage.html')
+
+@app.route('/table1')
+def table1():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     curs = connection.cursor()
-    curs.execute("select * from country, city, animal where (city.id = animal.id and animal.id = country.id and city.id = country.id and country.id <= 200 and country.country_name = 'China' and animal.gender='Male')")
+    curs.execute("select * from country, animal where (animal.id = country.id and country.country_name = 'China')")
     rows_ = curs.fetchall()
     connection.close()
-    return render_template('mainpage.html', rows = rows_)
+    return render_template('table1.html', rows = rows_)
+
+@app.route('/table2')
+def table2():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    curs = connection.cursor()
+    curs.execute("select * from country, animal where (animal.id = country.id and country.country_name = 'United States')")
+    rows_ = curs.fetchall()
+    connection.close()
+    return render_template('table2.html', rows = rows_)
+
+@app.route('/table3')
+def table3():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    curs = connection.cursor()
+    curs.execute("select * from country, animal where (animal.id = country.id and country.country_name = 'Brazil')")
+    rows_ = curs.fetchall()
+    connection.close()
+    return render_template('table3.html', rows = rows_)
+
+@app.route('/table4')
+def table4():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    curs = connection.cursor()
+    curs.execute("select * from country, animal where (animal.id = country.id and country.country_name = 'Canada')")
+    rows_ = curs.fetchall()
+    connection.close()
+    return render_template('table4.html', rows = rows_)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 # curs.execute("select * from city")
 # curs.execute("select * from animal")
-# curs.execute("select * from humans")
